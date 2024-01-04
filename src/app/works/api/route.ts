66fc81fe1c6df99ea6b21b2f5259433c37f03dc3
kici8 <artusi.daniele@gmail.com>
2024-01-04 18:35:24 +0100

    feat: add secret to revalidate

--- src/app/works/api/route.ts
+++ src/app/works/api/route.ts
@@ -1,12 +1,23 @@
 import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
 
 // https://nextjs.org/docs/pages/building-your-application/data-fetching/incremental-static-regeneration
 // Url example:
 // https://<your-site.com>/api/revalidate?secret=<token>
 // https://sandra-savor-next-isg.vercel.app/works/api?secret=1234
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
+  // TODO: with params I can get the path for the single work
+  // TODO: how to hendle the translation?
+  const secret = request.nextUrl.searchParams.get("secret");
+
+  if (secret !== process.env.MY_SECRET_TOKEN) {
+    return Response.json({ message: "Invalid token" }, { status: 400 });
+  }
+
+  // TODO: Path or Tag
   revalidatePath("/");
+  return Response.json({ revalidated: true, now: Date.now() });
 
   //   // Check for secret to confirm this is a valid request
   //   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
